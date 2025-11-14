827b23b1818089c9e1f123e5062a20ce0e053221
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-15 02:31:21 +0530

    ui/ux complete

---
+++ Snake Game/app.js
@@ -0,0 +1,12 @@
+const board = document.querySelector('.board');
+const blockHeight = 30;
+const blockWidth = 30;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+
+for(let i = 0; i < rows * cols; i++){
+    const block = document.createElement('div');
+    block.classList.add('block');
+    board.appendChild(block)
+}
